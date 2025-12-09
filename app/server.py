9d345d55dc06c8e4fc25b350c2822a1e440a9db9
GerBor <gerard.boruszewicz@gmail.com>
2025-12-09 18:39:51 +0100

    FIX

--- app/server.py
+++ app/server.py
@@ -1,1 +1,10 @@
-#base
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__, static_folder="../static", static_url_path="/")
+
+    @app.route("/")
+    def index():
+        return app.send_static_file("index.html")
+
+    return app
